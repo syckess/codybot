9349257f6abe2c6ce6cd027494be3e590e921f2b
syckess <sykesoncold@outlook.es>
2020-08-12 18:12:35 -0400

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
